92f889f1a7d7eedf6ffdd3382e3b81405afd1553
Casey Platt <kse0824@gmail.com>
2016-11-14 21:33:24 -0600

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_Garabaldi_Homeowners_Associationn_session'
---
+++ test/controllers/listings_controller_test.rb
@@ -0,0 +1,3 @@
+class ListingsControllerTest < ActionDispatch::IntegrationTest
+     
+end
