4f1c03d05fc9c14fcc21c58dfa717ff30946025d
Casey Platt <kse0824@gmail.com>
2016-11-15 18:03:18 -0600

    marathon all day

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,22 +9,37 @@ class ListingsController < ApplicationController
   end
 
   def create
-    render 'create.html.erb'
+    listing = Listing.new(name: params[:name], address: params[:address])
+    listing.save
+    #render 'create.html.erb'
+    #render 'create.html.erb'
+    redirect_to "/listings/#{listing.id}"
   end
 
   def show
+    @listing = Listing.find_by(id: params[:id]) 
     render 'show.html.erb'
   end
   
   def edit
+    @listing = Listing.find_by(id: params[:id])
     render 'edit.html.erb'
   end
 
   def update
-    render 'update.html.erb'
+    @listing = Listing.find_by(id: params[:id])
+    @listing.name = params[:name]
+    @listing.address = params[:body]
+    @listing.save
+    #render 'update.html.erb'
+    flash[:success] = "Listing successfully updated!"
+    redirect_to "/listings/#{listing.id}"
   end
 
   def destroy
-    render 'destroy.html.erb'
+    @listing = Listing.find_by(id: params[:id])
+    @listing.destroy
+    #render 'destroy.html.erb'
+    redirect_to "/listings/#{listing.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/listings/new' => 'listings#new'
   post '/listings' => 'listings#create'
   get '/listings/:id' => 'listings#show'
-  get '/listings/:id/edit' => 'listingss#edit'
+  get '/listings/:id/edit' => 'listings#edit'
   patch '/listings/:id' => 'listings#update'
   delete '/listings/:id' => 'listings#destroy'
 end
